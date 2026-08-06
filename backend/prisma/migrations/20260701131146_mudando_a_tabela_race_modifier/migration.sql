1c7f3edc624ac6db305240ee32b46cf3078f4d4e
Lucas-RibeiroSilva <lucascurso0711@gmail.com>
2026-08-06 11:54:32 -0300

    Delete backend directory

--- backend/prisma/migrations/20260701131146_mudando_a_tabela_race_modifier/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `operator` on the `RaceModifier` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "RaceModifier" DROP COLUMN "operator";
